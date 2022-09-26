e4475c247757d881be0825d557ddd75944953571
Khant Min Oakkar <95041341+KhantMinOakkar@users.noreply.github.com>
2022-09-26 23:04:29 +0630

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const currentPlace = document.querySelector(".currentPlace");
-const allPath = document.querySelectorAll(".mapContainer svg path");
-
-allPath.forEach(x=>{
-    x.addEventListener("mouseover",_=>{
-        currentPlace.innerText = x.getAttribute("title");
-    })
-})
